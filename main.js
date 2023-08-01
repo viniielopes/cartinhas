de977f07421204c1c76c078ef2c2ad03850a2705
Vinicius Lopes <viniielopes@live.com>
2023-08-01 20:26:49 -0300

    enhanced card layout and showing user info after fetch

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-const fetchPlayer = async (player)=> {
-    const res = await fetch(`http://localhost:3000/?search=${player}`);
+const fetchPlayer = async (player) => {
+    const res = await fetch(`http://localhost:3001/?search=${player}`);
     const data = await res.json();
 
     return data;
@@ -7,19 +7,30 @@ const fetchPlayer = async (player)=> {
 
 const btnEnviar = document.getElementById('enviar');
 
-btnEnviar.addEventListener('click', async ()=>{
+btnEnviar.addEventListener('click', async () => {
     const campoNome = document.getElementById("nome")
     const nome = campoNome.value;
     console.log(nome);
-    if(nome){
-        const playerData = await fetchPlayer(nome);
-        const campoCarta = document.querySelector(".carta");
-        
-        campoCarta.innerText = `Farm : ${playerData.farmLaner}
-        Ward/min, ${playerData.vi2}
-        Participação, ${playerData.partLane}
-        Dano/Min, ${playerData.danoTop}
-        Nota total, ${playerData.notaTop}`
+    if (nome) {
+        const { farmLaner, vi2, partLane, danoTop, notaTop, lane, tier, playerName } = await fetchPlayer(nome);
+        // const campoCarta = document.querySelector(".carta");
+        const campoNick = document.querySelector("#nick");
+        const campoNotaTotal = document.querySelector("#nota-total");
+        const campoElo = document.querySelector("#elo");
+        const campoLane = document.querySelector("#lane");
+        const campoVisao = document.querySelector("#visao");
+        const campoDano = document.querySelector("#dano");
+        const campoParticipacao = document.querySelector("#participacao");
+        const campoFarm = document.querySelector("#farm");
+
+        campoNick.innerText = playerName;
+        campoLane.innerText = lane;
+        campoElo.innerText = tier;
+        campoNotaTotal.innerText = notaTop;
+        campoVisao.innerText = vi2;
+        campoDano.innerText = danoTop;
+        campoParticipacao.innerText = partLane;
+        campoFarm.innerText = farmLaner;
     }
 })
 
