7af23945d6dcf2e4d8ba315524c0aa85f3a688b7
Vinicius Lopes <viniielopes@live.com>
2023-08-24 04:34:48 -0300

    refactor: changed .env variable name

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export const cardsAPI = axios.create({
   baseURL:
     process.env.NODE_ENV === "production"
-      ? process.env.CARDS_API
+      ? process.env.NEXT_PUBLIC_CARDS_API
       : "http://localhost:3001",
 });
